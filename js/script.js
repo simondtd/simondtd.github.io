661d7dd1d51cab9a9a2acb3c25e34794625a776d
s3694451 <Hesjeviksimon@gmail.com>
2021-04-08 13:08:54 +0200

    smooth scrolling

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 //Get the button:
 
 window.onload = function () {
-    mybutton = document.getElementById("scrollButton");
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    scrollButton = document.getElementById("scrollButton");
 
     // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function () { scrollFunction() };
 
     function scrollFunction() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
+            scrollButton.style.display = "block";
         } else {
-            mybutton.style.display = "none";
+            scrollButton.style.display = "none";
         }
     }
 }
